32ca55b88248648a0069d209f9b38f138062de84
RaslanAmir <amir.reslan.1978@gmail.com>
2025-09-18 10:39:13 +0200

    Explain internals visibility for tests

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 using System.Runtime.CompilerServices;
 
+// Allow the test project to interact with the DatabaseService test hooks without widening
+// their visibility beyond internal.
 [assembly: InternalsVisibleTo("YasGMP.Tests")]
