6bf968847e69a046e5672ad2995eb007f0b715e1
RaslanAmir <amir.reslan.1978@gmail.com>
2025-09-24 14:50:36 +0200

    docs: record batch0 dotnet retry status

--- YasGMP.Wpf/App.xaml.cs
+++ YasGMP.Wpf/App.xaml.cs
@@ -50,6 +50,8 @@ namespace YasGMP.Wpf
                         svc.AddSingleton<IDialogService, WpfDialogService>();
                         svc.AddSingleton<IFilePicker, WpfFilePicker>();
                         svc.AddSingleton<ICflDialogService, CflDialogService>();
+                        svc.AddSingleton<WorkOrderAuditService>();
+                        svc.AddTransient<WorkOrderService>();
                         svc.AddSingleton<ShellInteractionService>();
                         svc.AddSingleton<IModuleNavigationService>(sp => sp.GetRequiredService<ShellInteractionService>());
                         svc.AddSingleton<IShellInteractionService>(sp => sp.GetRequiredService<ShellInteractionService>());
